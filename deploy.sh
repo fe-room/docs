8b6652714b243f38fdbba308aa3b95dec3aefaf7
chenmeng <meng.chen@southgis.com>
2021-02-06 17:32:54 +0800

    更新配置

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:cs9507
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+ git push -f git@github.com:cs9507/Web-Docs.git main:gh-pages
 
 cd -
