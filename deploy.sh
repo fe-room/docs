7585ab58107f49b905b96a23cfad7a01d6552b20
chenmeng <meng.chen@southgis.com>
2021-02-07 11:49:15 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -16,10 +16,8 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:cs9507/docs.git main:gh-pages
+#git@github.com:cs9507/docs.git
+ git push -f git@github.com:cs9507/docs.git master:gh-pages
 
 cd -
