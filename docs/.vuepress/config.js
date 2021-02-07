3acda45e9ca51bb7f15328f102a15da56ec80ceb
chenmeng <meng.chen@southgis.com>
2021-02-07 11:24:48 +0800

    修改目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,5 @@
 module.exports = {
     title: 'Web Docs',
     description: 'Docs',
-    configureWebpack: {
-      resolve: {
-        alias: {}
-      }
-    },
-    base: "/Web-Docs/",
-    nav: [
-      {
-          text: 'Guide',
-          link: '/guide/',
-      }
-  ]
+    base: "/docs/"
   }
