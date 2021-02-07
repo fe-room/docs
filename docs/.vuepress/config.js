27bd869a109d716505c33eef6be5ffdf00d8b056
chenmeng <meng.chen@southgis.com>
2021-02-07 11:12:40 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,6 @@
 module.exports = {
     title: 'Web Docs',
     description: 'Docs',
-    theme: '@vuepress/blog',
-    themeConfig: {},
     configureWebpack: {
       resolve: {
         alias: {}
