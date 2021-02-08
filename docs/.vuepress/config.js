05f4b6a01274cb4cc690b6f35d6cff007b1048e7
chenmeng <meng.chen@southgis.com>
2021-02-08 10:30:37 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,31 @@
 module.exports = {
-  title: "Web Docs",
-  description: "Docs",
+  title: "Daily Notes",
+  description: "Programmer's daily notes",
   theme: "reco",
   base: "/docs/",
   themeConfig: {
-    // 博客配置
-   blogConfig: {
-     category: {
-       location: 2,     // 在导航栏菜单中所占的位置，默认2
-       text: 'Category' // 默认文案 “分类”
-     },
-     tag: {
-       location: 3,     // 在导航栏菜单中所占的位置，默认3
-       text: 'Tag'      // 默认文案 “标签”
-     },
-     socialLinks: [     // 信息栏展示社交信息
-       { icon: 'reco-github', link: 'https://github.com' },
-       { icon: 'reco-npm', link: 'https://www.npmjs.com' }
-     ]
-   }
- }
+    authorAvatar: '/hero.jpg',
+    author: 'admin',
+    startYear: '2021',
+    nav: [
+      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
+    ],
+    subSidebar: 'auto',//在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
+    type: 'blog',
+       // 博客配置
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag'      // 默认文案 “标签”
+      },
+    },
+
+  },
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ]
 };
--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,4 @@
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      | centered      |   $12 |
-| zebra stripes | are neat      |    $1 |
-
-测试一下travis
+---
+home: true
+---
 
